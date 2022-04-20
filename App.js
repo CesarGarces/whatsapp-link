620ef8f7420647c771046cec199182943036fdd7
cesar garces <cesargarce@gmail.com>
2022-04-20 17:07:39 -0500

    add files

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Linking,
+  TextInput,
+  Image,
+  useColorScheme,
+} from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import customData from "./codesData.json";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [colorScheme] = useState(useColorScheme());
+  const [items, setItems] = useState(customData);
+
+  console.log(items);
+  const handleClick = () => {
+    const numberLength = number.length;
+    const codeLength = value.length;
+    if (numberLength > 8 && codeLength > 0) {
+      Linking.openURL(`https://api.whatsapp.com/send?phone=${value}${number}`);
+    } else {
+      alert("Please enter a valid number");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={
+        colorScheme === "dark" ? styles.backgroundDark : styles.backgroundLigth
+      }
+    >
+      <Image
+        style={{ width: 100, height: 100, marginBottom: 5 }}
+        source={require("./assets/adaptive-icon.png")}
+      />
+      <Text style={{ fontSize: 15, marginBottom: 20 }}>
+        Envía whatsapp sin guardar contacto
+      </Text>
+      <View style={{ flexDirection: "col", width: "50%" }}>
+        <DropDownPicker
+          style={{
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+            borderBottomLeftRadius: 10,
+            borderBottomRightRadius: 10,
+            backgroundColor: "#676D67",
+          }}
+          dropDownStyle={{
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+          }}
+          searchable={true}
+          placeholder="Código País"
+          placeholderStyle={{ fontWeight: "bold" }}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
+        <Text style={styles.labelText}>Teléfono:</Text>
+        <TextInput
+          autoFocus={true}
+          autoCorrect={false}
+          autoComplete="off"
+          keyboardType="numeric"
+          onChangeText={e => setNumber(e)}
+          style={styles.inputNumber}
+        />
+      </View>
+      {value && number ? (
+        <Button
+          title="Enviar"
+          onPress={() => {
+            handleClick();
+          }}
+        />
+      ) : null}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundDark: {
+    backgroundColor: "#005C4B",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 100,
+  },
+  backgroundLigth: {
+    backgroundColor: "#fff",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 100,
+  },
+  inputNumber: {
+    borderWidth: 1,
+    borderColor: "#000",
+    marginTop: 20,
+    marginBottom: 20,
+    width: "100%",
+    height: 40,
+    fontSize: 20,
+  },
+  labelText: {
+    paddingTop: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "center",
   },
 });
